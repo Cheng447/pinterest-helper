1f75ed24cd288ede64f42ff63420c241443c3c41
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-08-20 21:35:23 +0800

    pinterest_helper

--- main.py
+++ main.py
@@ -17,30 +17,32 @@ class pinterest_helper(tk.Tk):
         self.iconbitmap('download_icon_128877.ico')
         self.resizable(False,False)
         self.configure(background='#1e191c')
-        self.entry1=tk.Entry(self,width=25)
+
+        self.sv=tk.StringVar()
+        self.sv.set("enter your path")
+
+        self.entry1=tk.Entry(self,width=25,textvariable=self.sv)
         self.entry1.pack(pady=(60,0))
-        self.open_button=tk.Button(self, text="open file",command=self.open_file_manager)
+
+        self.open_button=tk.Button(self, text="open file",command=self.open_file_manager,bd=3)
         self.open_button.pack(pady=15)
+
         self.entry2=tk.Entry(self,width=25)
         self.entry2.pack()
-        self.download_button=tk.Button(self,text="download")
+
+        self.download_button=tk.Button(self,text="download",bd=3)
         self.download_button.pack(pady=15)
 
         
     def open_file_manager(self):
         selected_path = filedialog.askdirectory()
         if selected_path:
-            self.entry1=tk.Entry(self,width=25,name=selected_path)
-            self.entry1.pack(pady=(60,0))
+            self.sv.set(f"{selected_path}")
+
+    def download(self):
+        ...
 
 ph=pinterest_helper()
+
 if __name__=="__main__":
     ph.mainloop()
-
-
-
-
-
-
-
-
